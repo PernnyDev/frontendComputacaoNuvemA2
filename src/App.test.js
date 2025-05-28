0c73706abe5246b93a45a49d27b4baa1ebdc8b0f
Vinicius Nascimento Ribeiro <vinicius.ribeiro44@fatec.sp.gov.br>
2025-05-27 22:01:06 -0300

    Remove arquivos e componentes não utilizados do projeto

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App component', () => {
-  render(<App />);
-  const headingElement = screen.getByRole('heading', { name: /Cadastro de Alunos/i });
-  expect(headingElement).toBeInTheDocument();
-});
